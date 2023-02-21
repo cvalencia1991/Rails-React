977cf08d282328dd1db758c6c6e05cec77e21ecb
cvalencia1991 <cesar4a6z@gmail.com>
2023-02-21 16:52:32 -0500

    Create random Api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+    Greeting.create(message: "Hello, world!")
+    Greeting.create(message: "Hi there!")
+    Greeting.create(message: "Greetings and salutations!")
+    Greeting.create(message: "Hey!")
+    Greeting.create(message: "Yo!")
